d765ca6cd675eba251cafd10643f837f53b2f693
Wagner Castro <102252795+OliveerCastro@users.noreply.github.com>
2022-10-19 10:08:53 -0300

    Create Flag-USA.js

---
+++ Javascript/Flag-USA.js
@@ -0,0 +1,10 @@
+let counter1 = 0;
+while (counter1 < 5) {
+ console.log("**********---------");
+ counter1++;
+}
+let counter2 = 0;
+while (counter2 < 4) {
+ console.log("-------------------");
+ counter2++;
+}
